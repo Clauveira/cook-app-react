1e8dfe45094375d8bbc8875f76f7c0c60e9e50c4
Clauveira <claue.silveira@gmail.com>
2024-03-04 21:13:54 -0300

    Estilização básica

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
 
+import { styles } from "./styles";
+
 export default function Index(){
     return(
-        <View>
-            <Text>Olá mundo.</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Escolha os produtos.</Text>
+            <Text style={styles.subtitle}>Descubra receitas baseadas nos produtos que você escolheu.</Text>
         </View>
     )
 }
